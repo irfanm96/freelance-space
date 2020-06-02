20f786172b178a28af86dce50d05443c148d5550
irfanmm96 <irfanmm96@gmail.com>
2020-06-02 15:25:00 +0530

    Emergency Updates

--- app/Jobs/InitProjectBoard.php
+++ app/Jobs/InitProjectBoard.php
@@ -97,6 +97,7 @@ class InitProjectBoard implements ShouldQueue
             'key' => $api_key,
             'description' => $description,
             'token' => $api_token,
+            // 'callbackURL' => route("project.webhook.$type", $this->project->id)
             'callbackURL' => route("project.webhook.$type", $this->project->id)
         ]);
         if ($response->failed()) {
