8737ab82e62bae617733d5600066d722ee7a5719
irfanmm96 <irfanmm96@gmail.com>
2020-06-02 15:36:33 +0530

    Emergency Updates

---
+++ app/Nova/Actions/DeleteWebhook.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Laravel\Nova\Actions\Action;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
+use Laravel\Nova\Fields\ActionFields;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class DeleteWebhook extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        foreach ($models as $webhook) {
+            $api_token = env('TRELLO_API_TOKEN');
+            $api_key = env('TRELLO_API_KEY');
+            $response = Http::delete("https://api.trello.com/1/webhooks/$webhook->webhook_id", [
+                'key' => $api_key,
+                'token' => $api_token,
+            ]);
+            if ($response->failed()) {
+                ld('webhook delete failed');
+                return;
+            }
+            $webhook->delete();
+            ld('webhook deleted sucessfully');
+            return;
+        }
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return [];
+    }
+}
