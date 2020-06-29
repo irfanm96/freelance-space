f701283bf211ca9b0d8869e57d456a94c73ad7b4
irfanmm96 <irfanmm96@gmail.com>
2020-06-29 12:26:50 +0530

    wip

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -67,6 +67,7 @@ class TaskController extends Controller
     private function handleUpdateCard($data)
     {
         $display = $data['display'];
+        unset($data['display']);
         if ($display['translationKey'] === 'action_move_card_from_list_to_list') {
             $list_after = $display['entities']['listAfter']['id'];
             $webhook = Webhook::where('list_id', $list_after)->first();
