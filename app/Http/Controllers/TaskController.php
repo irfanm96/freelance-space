bbad536ecc5be3dc7adc938945aa317413d185ac
irfanmm96 <irfanmm96@gmail.com>
2020-06-29 11:56:54 +0530

    wip

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -26,30 +26,35 @@ class TaskController extends Controller
     {
         $task = $this->processPayload($project_id, 'in_production', $request->input('action'));
         $this->handleCardAction($task);
+        return response('ok')->setStatusCode(200);
     }
 
     public function handleInStagingList($project_id, Request $request)
     {
         $task = $this->processPayload($project_id, 'in_staging', $request->input('action'));
         $this->handleCardAction($task);
+        return response('ok')->setStatusCode(200);
     }
 
     public function handleInProgressList($project_id, Request $request)
     {
         $task = $this->processPayload($project_id, 'in_progress', $request->input('action'));
         $this->handleCardAction($task);
+        return response('ok')->setStatusCode(200);
     }
 
     public function handleSprintBacklogList($project_id, Request $request)
     {
         $task = $this->handleCardAction($project_id, 'sprint_backlog', $request->input('action'));
         $this->handleCardAction($task);
+        return response('ok')->setStatusCode(200);
     }
 
     private function handleCreateCard($data)
     {
         unset($data['display']);
         Task::create($data);
+        ld('task created successfully');
     }
 
     private function handleUpdateCard($data)
@@ -68,6 +73,7 @@ class TaskController extends Controller
     {
         $action_type = $task['action_type'];
         unset($task['action_type']);
+        ld('handle card action', $action_type);
         switch ($action_type) {
             case 'createCard':
                 $this->handleCreateCard($task);
