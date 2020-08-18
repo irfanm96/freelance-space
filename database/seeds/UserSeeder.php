84bf9f2171da0e9f8a741b76fed525599e750737
irfanmm96 <irfanmm96@gmail.com>
2020-08-18 23:17:47 +0530

    wip

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,10 +19,29 @@ class UserSeeder extends Seeder
         ])->assignRole('super-admin');
 
         User::create([
-            'name' => 'Mohamed Fawzan',
-            'email' => 'fawzanm@gmail.com',
+            'name' => 'Another admin',
+            'email' => 'admin@example.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ])->assignRole('super-admin');
+
+        User::create([
+            'name' => 'Team Lead',
+            'email' => 'teamlead@example.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ])->assignRole('team-lead');
+
+        User::create([
+            'name' => 'Project owner',
+            'email' => 'projectowner@example.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ])->assignRole('project-owner');
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ])->assignRole('user');
+
         $roles = ['team-lead', 'project-owner', 'user'];
         factory(User::class, 50)->create()->each(function ($user) use ($roles) {
             $role = $roles[array_rand($roles)];
